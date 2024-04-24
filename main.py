eb8404b63ab4684fde2bd879778e1e4aa449ef28
Gad Marconi <gadmarconi4@outlook.com>
2024-04-24 17:11:26 +0200

    Refactor main.py to remove commented out code and improve code readability

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 
 from src.profiles import ProfileManager
-from src.scraper import get_jobs, Job
+from src.scraper import get_jobs
 from src.utils import Agent
+from src.agent import motivation_letter, profile_matcher
 
 load_dotenv()
 
@@ -12,14 +13,29 @@ def main():
 
     profiles = ProfileManager()
 
-    sample_job = Job(
-        agent,
-        "https://striive.com/nl/opdrachten/ministerie-van-economische-zaken-en-klimaat-dictu/senior-citrix-beheerder/ae1b2b31-a4ca-4cd9-8e46-c494a725ac50",
-        "Senior Citrix Administrator",
-        "Ministry of Economic Affairs and Climate Policy (DICTU)",
-    )
+    # sample_job = Job(
+    #     agent,
+    #     "https://striive.com/nl/opdrachten/ministerie-van-economische-zaken-en-klimaat-dictu/senior-citrix-beheerder/ae1b2b31-a4ca-4cd9-8e46-c494a725ac50",
+    #     "Senior Citrix Administrator",
+    #     "Ministry of Economic Affairs and Climate Policy (DICTU)",
+    # )
 
-    print(sample_job.assignment.skills, sample_job.assignment.requirements, sample_job.assignment.preferences)
+    jobs = get_jobs(agent)
+
+    matches = []
+
+    for job in jobs:
+        profile = profile_matcher(agent, profiles, job)
+        if profile:
+
+            data = {
+                "Position": job.position,
+                "Company": job.company,
+                "Candidate": profile,
+                "Motivation": motivation_letter(agent, profile, job),
+            }
+
+        matches.append(data)
 
 
 if __name__ == "__main__":
